66066a07bf169e16136455760f321dc4e7817a30
kapilkumar2001 <kapilkumar2001prajapat@gmail.com>
2021-03-05 09:34:06 +0530

    commit 3

---
+++ LinkedList/nth-node-from-end-of-linked-list.java
@@ -0,0 +1,103 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) 
+    {
+        data = d; 
+        next = null;
+    }
+}
+	
+
+	
+public class LinkedList_Element_From_Last
+{
+    Node head;  
+    Node tail;
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	  
+    public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+          
+        GfG g = new GfG(); 
+		System.out.println(g.getNthFromLast(llist.head,k));
+		
+			t--;
+		}
+	}
+}
+
+
+	
+
+
+class GfG
+{
+    int getNthFromLast(Node head, int n)
+    {
+    	Node start = head;
+    	Node end = start;
+    	
+    	while(n-->0)
+    	{
+    	    if(end.next!=null)
+    	    end = end.next;
+    	    else if(end.next==null && n==0)
+    	    {
+    	        return start.data;
+    	    }
+    	    else
+    	    return -1;
+    	}
+    	
+    	while(end!=null)
+    	{
+    	    start=start.next;
+    	    end = end.next;
+    	}
+    	
+    	return start.data;
+    }
+}
+
