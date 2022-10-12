fd35f059daf9361a92074c7d2b51faffc3d971d5
Kapil Kumar <56160115+kapilkumar2001@users.noreply.github.com>
2022-10-12 22:11:31 +0530

    Update arraypartition.cpp

--- Arrays/c++/arraypartition.cpp
+++ Arrays/c++/arraypartition.cpp
@@ -1,3 +1,6 @@
+// Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) 
+// such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
