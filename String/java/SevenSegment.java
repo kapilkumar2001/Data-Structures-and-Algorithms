035da6aa38d24c95272e771daa6752d52515b710
Azim Pathan <42502005+AzimPathan@users.noreply.github.com>
2022-10-12 23:53:53 +0530

    Updated file with problem statement

--- String/java/SevenSegment.java
+++ String/java/SevenSegment.java
@@ -1,5 +1,24 @@
 import java.util.*;
 
+/*
+
+PROBLEM:-
+Alice got a number written in seven segment format where each segment was creatted used a matchstick.
+
+Example: If Alice gets a number 123 so basically Alice used 12 matchsticks for this number.
+
+Alice is wondering what is the numerically largest value that she can generate by using at most the matchsticks that she currently possess.
+Help Alice out by telling her that number.
+
+Input Format:-
+First line contains T (test cases).
+Next T lines contain a Number N.
+
+Output Format:-
+Print the largest possible number numerically that can be generated using at max that many number of matchsticks which was used to generate N.
+
+*/
+
 public class SevenSegment {
     public static void main(String args[] ) throws Exception {
         Scanner sc = new Scanner(System.in);
